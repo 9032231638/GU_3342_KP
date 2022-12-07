691b461763741ae7d9ced8e9335f26a809fd4705
Alexander <9032231638a@gmail.com>
2022-12-07 05:46:50 +0300

    kp3

--- ИБ-ВС-КР/Program.cs
+++ ИБ-ВС-КР/Program.cs
@@ -3,18 +3,27 @@
 //  Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 //  При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
 
-string[] arr1 = new string[5] {"333", "ТРУД", "МАЙ", "МИР", "СТОЛ", "88"};
+string[] arr1 = new string[5] {"ТРУД", "88", "МАЙ", "ДА", "ВЕСНА"};
 string[] arr2 = new string[arr1.Length];
-
-void SecondArrayWithIF(string[] arr1, string[] arr2)
+void SecondArrayWithIF(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < arr1.Length; i++)
     {
-        if(arr1[i]).Length <= 3
+    if(arr1[i].Length <= 3)
         {
-            arr2[count] = arr1[i];
-            count++
+        arr2[count] = array1[i];
+        count++;
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(arr1, arr2);
+PrintArray(arr2);
